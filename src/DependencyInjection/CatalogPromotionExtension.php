54e6f18f82bfd4d7d3797a7310981ae081fa9e7c
Ahmed Kooli <kooliahmd@gmail.com>
2017-08-19 10:37:08 +0300

    auto loading sylius bundle configs

--- src/DependencyInjection/CatalogPromotionExtension.php
+++ src/DependencyInjection/CatalogPromotionExtension.php
@@ -11,16 +11,31 @@
 
 namespace SnakeTn\CatalogPromotion\DependencyInjection;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Yaml\Yaml;
 
-class CatalogPromotionExtension extends Extension
+class CatalogPromotionExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, \Symfony\Component\DependencyInjection\ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        $container->prependExtensionConfig(
+            'sylius_resource',
+            Yaml::parse(file_get_contents(__DIR__ . '/../Resources/config/app/resource.yml'))
+        );
+        $container->prependExtensionConfig(
+            'sylius_grid',
+            Yaml::parse(file_get_contents(__DIR__ . '/../Resources/config/app/grid.yml'))
+        );
     }
 
 
